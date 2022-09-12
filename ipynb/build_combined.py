bf41290e0ce17eb90e82fa3514d5814660b20514
Chang <cnk113@github.com>
2022-09-11 23:26:52 -0700

    added build

---
+++ ipynb/build_combined.py
@@ -0,0 +1,16 @@
+def build(filename, pca = False, umap = False):
+    import anndata
+    import pandas as pd
+    from itertools import chain
+    ad = anndata.read_mtx('combined.mtx')
+    #ad.layers['sct'] = anndata.read_mtx('sct.mtx').X
+    ad.layers['spliced'] = anndata.read_mtx('spliced.mtx').X
+    ad.layers['unspliced'] = anndata.read_mtx('unspliced.mtx').X
+    ad.obs = pd.read_csv('meta.csv',header=0)
+    if umap:
+        ad.obsm['X_umap'] = pd.read_csv('umap.csv',header=0).values
+    if pca:
+        ad.obsm['X_pca'] = pd.read_csv('pca.csv',header=0).values
+    ad.var_names = list(chain.from_iterable(pd.read_csv('genes.csv', header=0).values.tolist()))
+    ad.obs_names = list(chain.from_iterable(pd.read_csv('cells.csv', header=0).values.tolist()))
+    ad.write(filename)
